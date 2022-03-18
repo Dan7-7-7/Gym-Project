aabb803db579bfbde84b9352b2624dbffc1a9bc3
Dan7-7-7 <dannyranson19@gmail.com>
2022-03-18 12:12:21 +0000

    initial code for db directory

---
+++ db/gym_bookings.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS bookings
+DROP TABLE IF EXISTS classes
+DROP TABLE IF EXISTS members
+
+CREATE TABLE members (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    age INT
+);
+
+CREATE TABLE classes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    start_time INT,
+    duration INT,
+    capacity INT
+);
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    member_id INT REFERENCES members(id) ON DELETE CASCADE,
+    class_id INT REFERENCES classes(id) ON DELETE CASCADE
+);
