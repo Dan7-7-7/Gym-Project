57a0fd263eaeaaa2641a46d9d99f78eed6bdcc62
Dan7-7-7 <dannyranson19@gmail.com>
2022-03-18 12:18:44 +0000

    created models

---
+++ models/booking.py
@@ -0,0 +1,5 @@
+class Booking:
+    def __init__(self, member_name, class_name, id = None):
+        self.member_name = member_name
+        self.class_name = class_name
+        self.id = id
---
+++ models/class.py
@@ -0,0 +1,7 @@
+class Class:
+    def __init__(self, name, start_time, duration, capacity, id = None):
+        self.name = name
+        self.start_time = start_time
+        self.duration = duration
+        self.capacity = capacity
+        self.id = id
