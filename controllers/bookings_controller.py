5f230de3c9883d535049c5312a4569acf1514362
Dan7-7-7 <dannyranson19@gmail.com>
2022-03-19 17:33:39 +0000

    added show all classes

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -3,3 +3,7 @@ from models.booking import Booking
 from repositories import booking_repository
 
 bookings_blueprint = Blueprint("bookings", __name__)
+
+# all bookings
+# new booking
+# new booking for specific session - pre populated
--- controllers/sessions_controller.py
+++ controllers/sessions_controller.py
@@ -1,5 +1,27 @@
-from flask import Flask, Blueprint, render_template, request, redirect
+from flask import Flask, Blueprint, render_template, request, redirect, session
 from models.session import Session
 from repositories import session_repository
 
 sessions_blueprint = Blueprint("sessions", __name__)
+
+@sessions_blueprint.route('/classes')
+def show_classes():
+    sessions = session_repository.select_all()
+    return render_template('/sessions/index.html', title="Classes", sessions=sessions)
+
+# @sessions_blueprint.route('/classes/new')
+
+
+# @sessions_blueprint.route('/classes', methods=['POST'])
+
+
+# @sessions_blueprint.route('/classes/<id>')
+
+
+# @sessions_blueprint.route('/classes/<id>/edit')
+
+
+# @sessions_blueprint.route('/classes/<id>', methods=['POST'])
+
+
+# @sessions_blueprint.route('/classes/<id>/members')
